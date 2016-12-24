21795aa857f736497e78d167bdd203d9b2f9cc67
OhnoYutaro <utr066@yahoo.co.jp>
2016-12-24 12:32:23 +0900

    型の変換が謎ですね〜。

--- simple_calculation/ViewController.swift
+++ simple_calculation/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var answer: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
+    @IBAction func numberTapped(_ sender: UIButton) {
+        answer.text! += sender.currentTitle!
+        
+    }
+
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
